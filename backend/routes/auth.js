5f33a776307eef26a9b363d7f6a9b41bb06ae226
Abdullah <mafishere@gmail.com>
2024-08-17 17:14:15 +0500

    get

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { signup, login, logout, check_auth } = require("../controllers/authController.js");
+const {
+  signup,
+  login,
+  logout,
+  check_auth,
+} = require("../controllers/authController.js");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.get("/logout", logout);
 
 module.exports = router;
